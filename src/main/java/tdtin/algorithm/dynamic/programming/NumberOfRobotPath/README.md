633ead116772ddf4b8e75f235569c4ccf3245ab5
Tin Tran <tdtin95@gmail.com>
2022-10-04 23:12:15 +0800

    Number of Robot Paths

---
+++ src/main/java/tdtin/algorithm/dynamic/programming/NumberOfRobotPath/README.md
@@ -0,0 +1,24 @@
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+**Example 1:**
+
+Input: m = 2, n = 3
+
+Output: 3
+
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+
+Right -> Right -> Down
+
+Right -> Down -> Right
+
+Down -> Right -> Right
+
+**Example 2:**
+
+Input: m = 5, n = 3
+
+Output: 15
---
+++ src/main/java/tdtin/algorithm/dynamic/programming/NumberOfRobotPath/Solution.java
@@ -0,0 +1,28 @@
+package tdtin.algorithm.dynamic.programming.NumberOfRobotPath;
+
+import java.util.Scanner;
+
+class Solution {
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[n][m];
+        for (int i = 0 ; i < n; i++) {
+            for (int j = 0; j < m ; j++) {
+                if (i == 0 || j ==0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+        int res = uniquePaths(m, n);
+        System.out.println(res);
+    }
+}
